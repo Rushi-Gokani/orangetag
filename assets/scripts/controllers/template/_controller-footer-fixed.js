162237c2cfecab4078785f6667d506452ba8e718
Rushi Gokani <Rushi-Gokani>
2025-07-02 13:56:14 +0530

    Initial commit

---
+++ assets/scripts/controllers/template/_controller-footer-fixed.js
@@ -0,0 +1,42 @@
+/***********************************************
+ * TEMPLATE: FOOTER FIXED
+ ***********************************************/
+(function ($) {
+
+	'use strict';
+
+	if (!$('.ptf-is--footer-fixed').length) {
+		return;
+	}
+
+	if (PTFJS.isMobile.any()) {
+		return;
+	}
+
+	PTFJS.fixedFooter = {
+		init: function () {
+
+			var footer = $('.ptf-footer');
+
+			PTFJS.window.on('load resize', function () {
+				var footerHeight = footer.outerHeight();
+				if (footerHeight <= PTFJS.window.height()) {
+					var leftValue = PTFJS.body.css('padding-left');
+					footer.css({
+						'left': leftValue,
+						'right': '0',
+						'bottom': '0'
+					});
+				} else {
+					PTFJS.body.css('padding-bottom', 0);
+					footer.removeAttr('style');
+				}
+
+			});
+
+		}
+	};
+
+	PTFJS.fixedFooter.init();
+
+})(jQuery);
